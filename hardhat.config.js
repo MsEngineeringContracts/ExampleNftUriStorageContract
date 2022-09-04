faa862453e4ae5146e4fadf61e569201b21e7654
Mathias Schneider <112683354+MathiasSchneider86@users.noreply.github.com>
2022-09-04 11:06:41 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require('@openzeppelin/hardhat-upgrades');
 require("hardhat-gas-reporter");
 require("@nomiclabs/hardhat-web3");
 require('hardhat-deploy');
+require('dotenv').config({ path: require('find-config')('.env') });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -30,21 +32,29 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
   	localhost: {
-      url: "http://your.ip:port"
+      url: "http://127.0.0.1:8545"
     },
     hardhat: {
     },
     tbsc: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      from: process.env.ADDRESS_GFNG_INIT,
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: { mnemonic:  ""}
+      accounts: { mnemonic:  process.env.MNEMONIC_GFNG_INIT}
     },
     bsc: {
       url: "https://bsc-dataseed.binance.org/",
+      from: process.env.ADDRESS_GFNG_INIT,
       chainId: 56,
       gasPrice: 20000000000,
-      accounts: { mnemonic:  ""}
+      accounts: { mnemonic:  process.env.MNEMONIC_GFNG_INIT}
+    },
+    tmatic: {
+      url: "https://matic-mumbai.chainstacklabs.com",
+      from: process.env.ADDRESS_GFNG_INIT,
+      chainId: 80001,
+      accounts: { mnemonic: process.env.MNEMONIC_GFNG_INIT}
     }
   },
   gasReporter: {
@@ -52,8 +62,14 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
-      bscTestnet: ""
+      bsc: process.env.BSCSCAN_API_TOKEN,
+      bscTestnet: process.env.BSCSCAN_API_TOKEN,
+      polygon: process.env.POLYGONSCAN_API_TOKEN,
+      polygonMumbai: process.env.POLYGONSCAN_API_TOKEN      
     }
+  },
+  mocha:{
+    timeout: 120000 
   }
 };
 
